d8439929384852b868bb547ec7e5083f0ce1ee40
Anselme <anselmedupuy@gmail.com>
2024-09-25 12:16:58 +0200

    Commit ex-4

---
+++ ex-4/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	main	() {
+	int var1 = 6;
+	int var2 = 2;
+	int fin = var1 * var2;
+
+	char nom[10] = "Anselme";
+
+	printf("%d x %d = %d\n", var1, var2, fin);
+	printf("%s\n", nom);
+	exit(0);
+
+}
