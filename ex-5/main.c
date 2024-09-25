afc8ca9b8abd454ac0bdb2a57a0129c9520a32cb
Anselme <anselmedupuy@gmail.com>
2024-09-25 13:43:26 +0200

    commit ex-5

---
+++ ex-5/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main () {
+	
+	float A = 12;
+	float B = 7;
+
+	float result = (A + B) /2;
+
+
+	printf("La moyenne de 12 et 7 est égale à %.2f\n", result);
+
+
+	
+}
